56b7f737d28f38d88f4b3639edf05037526370e2
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2022-07-24 11:42:43 +0530

    commit

---
+++ AccessModifiers/Simple.java
@@ -0,0 +1,9 @@
+class A{  
+private A(){}//private constructor  
+void msg(){System.out.println("Hello java");}  
+}  
+public class Simple{  
+ public static void main(String args[]){  
+   A obj=new A();//Compile Time Error  
+ }  
+} 
