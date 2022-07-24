c7fbd22188cb410eb4f4dc6c5fa65cd046f1a67b
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2022-07-24 13:47:15 +0530

    commit

---
+++ StringMethods/TeststringComparison2.java
@@ -0,0 +1,9 @@
+class TeststringComparison2{  
+ public static void main(String args[]){  
+   String s1="Sachin";  
+   String s2="SACHIN";  
+  
+   System.out.println(s1.equals(s2));//false  
+   System.out.println(s1.equalsIgnoreCase(s2));//true  
+ }  
+} 
