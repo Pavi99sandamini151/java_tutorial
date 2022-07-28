3df41aea96ed6e125f7ca631e1e8c5382db21bbe
Pavi99sandamini151 <84126210+Pavi99sandamini151@users.noreply.github.com>
2022-07-28 15:07:19 +0530

    Add files via upload

---
+++ StructuralDesignPatterns/Adapter pattern/CreditCard.java
@@ -0,0 +1,4 @@
+public interface CreditCard {  
+    public void giveBankDetails();  
+    public String getCreditCard();  
+}// End of the CreditCard interface.
---
+++ StructuralDesignPatterns/Bridge pattern/QuestionFomat.java
@@ -0,0 +1,11 @@
+// this is the QuestionFormat class.  
+public class QuestionFormat extends QuestionManager {  
+    public QuestionFormat(String catalog){  
+        super(catalog);  
+    }  
+    public void displayAll() {  
+        System.out.println("\n---------------------------------------------------------");  
+        super.displayAll();  
+        System.out.println("-----------------------------------------------------------");  
+    }  
+}// End of the QuestionFormat class.  
